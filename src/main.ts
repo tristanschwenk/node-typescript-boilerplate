933ca54ac799042b263fa4a9b369fcbb5524e37d
Tristan264 <tristan.bschwenk@icloud.com>
2025-04-08 15:41:24 +0200

    feat: move fizzbuzz to external function

--- src/main.ts
+++ src/main.ts
@@ -11,15 +11,19 @@ export const expo = (a, b) => a ** b;
 export const fizzBuzz = (n): (number | string)[] => {
   const fizzBuzzArray = [];
   for (let i = 1; i <= n; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      fizzBuzzArray.push('FizzBuzz');
-    } else if (i % 3 === 0) {
-      fizzBuzzArray.push('Fizz');
-    } else if (i % 5 === 0) {
-      fizzBuzzArray.push('Buzz');
-    } else {
-      fizzBuzzArray.push(i);
-    }
+    fizzBuzzArray.push(checkFixBuzz(i));
   }
   return fizzBuzzArray;
+};
+
+export const checkFixBuzz = (n: number): number | string => {
+  if (n % 3 === 0 && n % 5 === 0) {
+    return 'FizzBuzz';
+  } else if (n % 3 === 0) {
+    return 'Fizz';
+  } else if (n % 5 === 0) {
+    return 'Buzz';
+  } else {
+    return n;
+  }
 };
