5d702d799a7650b3e392efea39efc3b780b4bc61
Tristan264 <tristan.bschwenk@icloud.com>
2025-04-08 15:41:58 +0200

    feat: handle spare and strike

--- src/game.ts
+++ src/game.ts
@@ -1,14 +1,14 @@
 export class Game {
-  private frame: number;
-  private pins: number;
-  private currentScore: number;
-  private step: number;
-  private hadStrike: boolean;
-  private hadSpare: boolean;
+  public frame: number;
+  public pins: number;
+  public currentScore: number;
+  public step: number;
+  public hadStrike: boolean;
+  public hadSpare: boolean;
 
   constructor() {
     this.frame = 0;
-    this.pins = 0;
+    this.pins = 10;
     this.currentScore = 0;
     this.step = 0;
     this.hadSpare = false;
@@ -31,38 +31,50 @@ export class Game {
       this.handleStrike(pins);
       return;
     }
+    if (this.hadSpare) {
+      this.handlePreviousSpare(pins);
+    }
     this.step++;
     this.handleRoll(pins);
   }
 
   public handleSecondStep(pins: number): void {
-    if (this.pins + pins === 10) {
+    if (this.pins - pins === 0) {
+      console.log('spare');
       this.handleSpare(pins);
       return;
     }
-    this.step = 0;
-    this.frame++;
+    this.resetFrame();
     this.handleRoll(pins);
   }
 
   public handleStrike(pins: number): void {
     this.hadStrike = true;
-    this.step = 0;
-    this.frame++;
     this.handleRoll(pins);
+    this.resetFrame();
   }
 
   public handleSpare(pins: number): void {
     this.hadSpare = true;
-    this.step = 0;
-    this.frame++;
     this.handleRoll(pins);
+    this.resetFrame();
+  }
+
+  public handlePreviousSpare(pins: number): void {
+    const bonus = pins;
+    this.hadSpare = false;
+    this.handleRoll(bonus + pins);
   }
 
   public handleRoll(pins: number): void {
     this.pins -= pins;
-    this.frame++;
     this.currentScore += pins;
+  }
+
+  public resetFrame(): void {
+    this.step = 0;
+    this.frame++;
+    this.pins = 10;
   }
 
   public score(): number {
